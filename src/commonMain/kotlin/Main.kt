44017e7dbfb04d7dfcb1452fcd26ed3a42859041
soywiz <soywiz@gmail.com>
2023-11-26 13:18:29 +0100

    Initial PieChart backported from @rafi0
    From PR: https://github.com/korlibs/korge/pull/2027

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import korlibs.korge.*
 import korlibs.korge.scene.*
 import korlibs.korge.view.*
+import korlibs.korge.datapresentation.*
 import org.korge.samples.mymodule.*
 
 suspend fun main() = Korge {
@@ -9,6 +10,29 @@ suspend fun main() = Korge {
 
 class MainMyModuleScene : Scene() {
     override suspend fun SContainer.sceneMain() {
-        text(MyModule.TEXT)
+        val pie = pieChart(70f) {
+            position(100, 100)
+        }
+
+        pie.setColors(
+            listOf(
+                Colors["#73ff5f"],
+                Colors["#4058ff"],
+                Colors["#1efff8"],
+                Colors["#fff918"],
+            )
+        )
+        pie.updateData(
+            listOf(
+                "example" to 10f,
+                "another" to 30f,
+                "very cool" to 5f,
+                "exampleeee" to 15f,
+            )
+        )
+        pie.getParts()[2].apply {
+            onOver { colorMul = Colors.BLACK }
+            onOut { colorMul = Colors.WHITE }
+        }
     }
 }
